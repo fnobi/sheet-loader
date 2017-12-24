91121f9dd09197079c783d860b52b45a3f23a966
nobii <fujisawa-shin@kayac.com>
2017-12-24 22:10:50 +0900

    npm見直し・class記法使う

--- index.js
+++ index.js
@@ -1,104 +1,103 @@
 var read = require('read');
-
 var GoogleSpreadsheet = require('google-spreadsheet');
 var googleAuth = require("google-auth-library");
-var _ = require('underscore');
+var _ = require('lodash');
 
-var SheetLoader = function (opts) {
-    opts = opts || {};
+class SheetLoader {
+    constructor (opts = {}) {
+        this.sheetKey = opts.sheetKey;
+        this.sheetTitle = opts.sheetTitle;
+        this.keyFilePath = opts.keyFilePath;
+        this.serviceAccount = opts.serviceAccount;
+        this.columns = opts.columns || {};
+    }
 
-    this.sheetKey = opts.sheetKey;
-    this.sheetTitle = opts.sheetTitle;
-    this.keyFilePath = opts.keyFilePath;
-    this.serviceAccount = opts.serviceAccount;
-    this.columns = opts.columns || {};
-};
-
-SheetLoader.prototype.load = function (opts = {}) {
-    return Promise.resolve().then(() => {
-        return new Promise((resolve, reject) => {
-            if (!this.sheetKey) {
-                reject(new Error('invalid sheet key.'));
-                return;
-            }
-            if (!this.sheetTitle) {
-                reject(new Error('invalid sheet title.'));
-                return;
-            }
-            resolve();
-        })
-    }).then(() => {
-        return new Promise((resolve, reject) => {
-            var authClient = new googleAuth();
-            var jwtClient = new authClient.JWT(
-                this.serviceAccount,
-                this.keyFilePath,
-                null,
-                ["https://spreadsheets.google.com/feeds"],
-                null
-            );
-            jwtClient.authorize((err, token) => {
-                if (err) {
-                    reject(err);
+    load () {
+        return Promise.resolve().then(() => {
+            return new Promise((resolve, reject) => {
+                if (!this.sheetKey) {
+                    reject(new Error('invalid sheet key.'));
                     return;
                 }
-                var book = new GoogleSpreadsheet(this.sheetKey, {
-                    type: token.token_type,
-                    value: token.access_token
-                });
-                resolve(book);
-            });
-        });
-    }).then((book) => {
-        return new Promise((resolve, reject) => {
-            book.getInfo(function (err, result) {
-                if (err) {
-                    reject(err);
+                if (!this.sheetTitle) {
+                    reject(new Error('invalid sheet title.'));
                     return;
                 }
-                resolve(result);
-            });
-        });
-    }).then((bookInfo) => {
-        return new Promise((resolve, reject) => {
-            // TODO: lodash#find 使いたい
-            var worksheet;
-            bookInfo.worksheets.forEach((s) => {
-                if (s.title == this.sheetTitle) {
-                    worksheet = s;
-                }
-            });
-            if (!worksheet) {
-                reject(new Error('sheet "' + this.sheetTitle + '" is not found.'));
-                return;
-            }
-            resolve(worksheet);
-        });
-    }).then((worksheet) => {
-        return new Promise((resolve, reject) => {
-            worksheet.getRows(function (err, result) {
-                if (err) {
-                    reject(err);
-                    return;
-                }
-                resolve(result);
-            });
-        });
-    }).then((rows) => {
-        return new Promise((resolve, reject) => {
-            var arr = [];
-            rows.forEach((row) => {
-                var obj = {};
-                for (var key in this.columns) {
-                    if (row[this.columns[key]]) {
-                        obj[key] = row[this.columns[key]];
+                resolve();
+            })
+        }).then(() => {
+            return new Promise((resolve, reject) => {
+                var authClient = new googleAuth();
+                var jwtClient = new authClient.JWT(
+                    this.serviceAccount,
+                    this.keyFilePath,
+                    null,
+                    ["https://spreadsheets.google.com/feeds"],
+                    null
+                );
+                jwtClient.authorize((err, token) => {
+                    if (err) {
+                        reject(err);
+                        return;
                     }
-                }
-                arr.push(obj);
+                    var book = new GoogleSpreadsheet(this.sheetKey, {
+                        type: token.token_type,
+                        value: token.access_token
+                    });
+                    resolve(book);
+                });
             });
-            resolve(arr);
+        }).then((book) => {
+            return new Promise((resolve, reject) => {
+                book.getInfo(function (err, result) {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+                    resolve(result);
+                });
+            });
+        }).then((bookInfo) => {
+            return new Promise((resolve, reject) => {
+                // TODO: lodash#find 使いたい
+                var worksheet;
+                bookInfo.worksheets.forEach((s) => {
+                    if (s.title == this.sheetTitle) {
+                        worksheet = s;
+                    }
+                });
+                if (!worksheet) {
+                    reject(new Error('sheet "' + this.sheetTitle + '" is not found.'));
+                    return;
+                }
+                resolve(worksheet);
+            });
+        }).then((worksheet) => {
+            return new Promise((resolve, reject) => {
+                worksheet.getRows(function (err, result) {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+                    resolve(result);
+                });
+            });
+        }).then((rows) => {
+            return new Promise((resolve, reject) => {
+                var arr = [];
+                rows.forEach((row) => {
+                    var obj = {};
+                    for (var key in this.columns) {
+                        if (row[this.columns[key]]) {
+                            obj[key] = row[this.columns[key]];
+                        }
+                    }
+                    arr.push(obj);
+                });
+                resolve(arr);
+            });
         });
-    });
-};
+    }
+}
 
 module.exports = SheetLoader;
